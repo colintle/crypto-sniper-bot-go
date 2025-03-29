ade3e1b0a3fec6863de22756ea587dfabcb256bf
Colin Le <Colin.Le@ucf.edu>
2025-03-29 15:45:35 -0400

    Add constants of the app

---
+++ config/constants.go
@@ -0,0 +1,22 @@
+package config
+
+import "os"
+
+const (
+	RISK_TOLERANCE = 0.50
+	SLIPPAGE        = 1
+
+	JUPITER_QUOTE_URL = "https://api.jup.ag/swap/v1/quote"
+	JUPITER_SWAP_URL  = "https://api.jup.ag/swap/v1/swap"
+
+	SOLANA_RPC_URL = "https://api.mainnet-beta.solana.com"
+	SOL_MINT        = "So11111111111111111111111111111111111111112"
+	LAMPORTS_PER_SOL = 1_000_000_000
+)
+
+var (
+	PUBLIC_KEY_WALLET  = os.Getenv("PUBLIC_KEY_WALLET")
+	BASE58_PRIVATE_KEY = os.Getenv("PRIVATE_KEY_WALLET")
+	TRACKING_WALLET    = os.Getenv("TRACKING_WALLET")
+	API_KEY            = os.Getenv("HELIUS_API_KEY")
+)
