ab5a5fd75c3ad8b20f8fd4eb7d1cf6362c5ef00c
Colin Le <colintranle123@gmail.com>
2025-03-31 11:09:27 -0400

    Handle deleting keys at start of redis database

--- main.go
+++ main.go
@@ -4,23 +4,35 @@ import (
 	"log"
 	"net/http"
 
-	// "github.com/colintle/crypto-sniper-bot-go/handlers/helius"
-	"github.com/colintle/crypto-sniper-bot-go/handlers/helloWorld"
 	"github.com/colintle/crypto-sniper-bot-go/database"
+	"github.com/colintle/crypto-sniper-bot-go/handlers/helius"
+	"github.com/colintle/crypto-sniper-bot-go/handlers/helius/logging"
+	"github.com/colintle/crypto-sniper-bot-go/handlers/helius/transaction"
+	"github.com/colintle/crypto-sniper-bot-go/handlers/helloWorld"
 )
 
-func main(){
+func main() {
 
-	success := database.InitializeRedis()
+	var success bool
 
-	if !success{
+	success = database.InitializeRedis()
+	if !success {
 		log.Fatal("Redis is not connected")
 	}
 
 	log.Println("Redis is connected!")
 
+	success = database.DeleteAllRedis()
+	if !success {
+		log.Fatal("Not able to delete all in Redis")
+	}
+
+	logging.StartCSVLogger(100)
+	defer logging.StopCSVLogger()
+
+	transaction.InitKeypair()
 	http.HandleFunc("/", helloWorld.HelloWorldHandler)
-	//http.HandleFunc("/helius", helius.HeliusHandler)
+	http.HandleFunc("/helius", helius.HeliusHandler)
 
 	log.Println("Server running on :5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
