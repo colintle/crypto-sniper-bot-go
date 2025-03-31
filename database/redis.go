f8c6d028c919c3a188eb87495da84bf0549cbefe
Colin Le <colintranle123@gmail.com>
2025-03-31 11:08:52 -0400

    All redis related

--- database/redis.go
+++ database/redis.go
@@ -2,8 +2,8 @@ package database
 
 import (
 	"context"
-	"log"
 	"fmt"
+	"log"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -13,26 +13,37 @@ var (
 	rdb *redis.Client
 )
 
-func GetRedisClient() *redis.Client{
+func GetRedisClient() *redis.Client {
 	return rdb
 }
 
-func GetRedisContext() context.Context{
+func GetRedisContext() context.Context {
 	return ctx
 }
 
-func InitializeRedis() bool{
+func InitializeRedis() bool {
 	rdb = redis.NewClient(&redis.Options{
 		Addr: "redis:6379",
 	})
 
 	_, err := rdb.Ping(ctx).Result()
 
-	if err != nil{
-		log.Fatal("Couldn't connect to Redis: %v", err)
+	if err != nil {
+		log.Fatalf("Couldn't connect to Redis: %v", err)
 		return false
 	}
 
 	fmt.Println("Connected to Redis")
 	return true
+}
+
+func DeleteAllRedis() bool {
+	err := rdb.FlushDB(ctx).Err()
+	if err != nil {
+		log.Printf("Failed to flush Redis DB: %v", err)
+		return false
+	} else {
+		log.Println("Redis DB flushed successfully")
+		return true
+	}
 }
