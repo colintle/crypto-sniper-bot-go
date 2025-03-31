e5ddca1c167a79544e21ff7d98c798c7a9305b55
Colin Le <colintranle123@gmail.com>
2025-03-31 12:25:43 -0400

    Testing code at helloworld endpoint

--- handlers/helloWorld/helloWorld.go
+++ handlers/helloWorld/helloWorld.go
@@ -1,12 +1,30 @@
 package helloWorld
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+
+	"github.com/colintle/crypto-sniper-bot-go/config"
+	"github.com/colintle/crypto-sniper-bot-go/handlers/helius/transaction"
+	"github.com/colintle/crypto-sniper-bot-go/models"
 )
 
-func HelloWorldHandler(w http.ResponseWriter, r *http.Request){
+func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Testing")
 	fmt.Fprintf(w, "Hello World!")
+
+	var result models.Trade
+	result = transaction.Buy("7HoCDyzPwSfgrPxYCA5w1XetujmBtDhUPUnTrEF1pump", 0.05)
+	if result.Success {
+		fmt.Fprintf(w, "Transaction went through")
+		result1 := transaction.Sell("7HoCDyzPwSfgrPxYCA5w1XetujmBtDhUPUnTrEF1pump", *result.AmountToken*config.POSITION_SIZE)
+		if result1.Success {
+			fmt.Fprintf(w, "Buy and Sell went through")
+		} else {
+			fmt.Fprintf(w, "Buy works but sell didn't")
+		}
+	} else {
+		fmt.Fprintf(w, "Neither buy or sell works")
+	}
 }
